a88b4df1255778e2079d2004e54de4576b6b6742
root <root@sulis15.zcu.cz>
2026-04-13 10:51:11 +0200

    initial

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"os/exec"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+func getWindowsVersion() string {
+	cmd := exec.Command("cmd", "/C", "ver")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "Chyba při zjišťování verze"
+	}
+	return out.String()
+}
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Windows verze")
+
+	label := widget.NewLabel("Klikni na tlačítko")
+
+	button := widget.NewButton("Zjistit verzi", func() {
+		label.SetText(getWindowsVersion())
+	})
+
+	w.SetContent(widget.NewVBox(label, button))
+	w.Resize(fyne.NewSize(300, 200))
+	w.ShowAndRun()
+}
